100df03ce3b1cd86e1c2401cd53527a6a5b766d2
Jamie Tartell <jetartell@gmail.com>
2020-02-24 19:04:39 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,20 @@
+function shout(string) {
+  return string.toUpperCase(string)
+  console.log(string)
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+
+
+
+
